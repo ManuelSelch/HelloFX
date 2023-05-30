82762d9b32499c825fa95264e8cc459988a56b61
Manuel Selch <manuel@selch.de>
2023-05-30 20:19:19 +0200

    HelloFX demo

---
+++ target/gluonfx/arm64-ios/gvm/HelloFX/hellofx.hellofx.h
@@ -0,0 +1,16 @@
+#ifndef __HELLOFX_HELLOFX_H
+#define __HELLOFX_HELLOFX_H
+
+#include <graal_isolate.h>
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+int run_main(int argc, char** argv);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
---
+++ target/gluonfx/arm64-ios/gvm/HelloFX/hellofx.hellofx_dynamic.h
@@ -0,0 +1,16 @@
+#ifndef __HELLOFX_HELLOFX_H
+#define __HELLOFX_HELLOFX_H
+
+#include <graal_isolate_dynamic.h>
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef int (*run_main_fn_t)(int argc, char** argv);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
